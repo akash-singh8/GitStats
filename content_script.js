832044971c56ab2b2365f0fb18a266a3853f0910
akash-singh8 <developer.akash8@gmail.com>
2023-05-19 22:26:19 +0530

    content script - send user-id to popup

--- content_script.js
+++ content_script.js
@@ -1,1 +1,7 @@
 console.log("Hello World");
+
+// Receive message from popup & send user_name in response
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  console.log("Query :", message);
+  sendResponse({ user: window.location.pathname.split("/")[1] });
+});
--- popup.js
+++ popup.js
@@ -39,47 +39,68 @@ function displayUserStats(user) {
     .then((data) => {
       console.log("Data :", data);
 
-      document.querySelector(".user_not_found").style.display = "none";
+      const type = data?.type;
+      if (type === "User" || !type) {
+        document.querySelector(".user_not_found").style.display = "none";
 
-      let section = document.querySelector("section");
-      if (!section) {
-        section = document.createElement("section");
-        document.querySelector("body").appendChild(section);
-      }
-      section.innerHTML = `
-      <div class="user_details">
-        <img
-          src=${data.avatar_url}
-          alt="avatar" />
-        <div>
-          <p id="user_name">${data.name}</p>
-          <p id="user_id">${user}</p>
-          <p><strong id="user_repo">${data.public_repos}</strong> repositories</p>
+        let section = document.querySelector("section");
+        if (!section) {
+          section = document.createElement("section");
+          document.querySelector("body").appendChild(section);
+        }
+        section.innerHTML = `
+        <div class="user_details">
+          <img
+            src=${data.avatar_url}
+            alt="avatar" />
+          <div>
+            <p id="user_name">${data.name}</p>
+            <p id="user_id">${user}</p>
+            <p><strong id="user_repo">${data.public_repos}</strong> repositories</p>
+          </div>
         </div>
-      </div>
-
-      <img
-        src="https://github-readme-streak-stats.herokuapp.com?user=${user}&theme=dracula&hide_border=true&card_width=450"
-        alt="Streak stats"
-        class="streak_stats" />
-
-      <div class="used_languages">
-        <p>
-          MOST <br />
-          USED <br />
-          LANGS
-        </p>
+  
         <img
-          src="https://github-readme-stats.vercel.app/api/top-langs?username=${user}&show_icons=true&locale=en&layout=compact&theme=dracula&hide_border=true&hide_title=true"
-          alt="Most used languages" />
-      </div>
-
-      <img
-        src="https://github-readme-stats.vercel.app/api?username=${user}&theme=dracula&include_all_commits=true&count_private=true&show_icons=true&hide=contribs,commits&hide_border=true&hide_title=true"
-        alt="Github stats"
-        class="profile_stats" /> `;
+          src="https://github-readme-streak-stats.herokuapp.com?user=${user}&theme=dracula&hide_border=true&card_width=450"
+          alt="Streak stats"
+          class="streak_stats" />
+  
+        <div class="used_languages">
+          <p>
+            MOST <br />
+            USED <br />
+            LANGS
+          </p>
+          <img
+            src="https://github-readme-stats.vercel.app/api/top-langs?username=${user}&show_icons=true&locale=en&layout=compact&theme=dracula&hide_border=true&hide_title=true"
+            alt="Most used languages" />
+        </div>
+  
+        <img
+          src="https://github-readme-stats.vercel.app/api?username=${user}&theme=dracula&include_all_commits=true&count_private=true&show_icons=true&hide=contribs,commits&hide_border=true&hide_title=true"
+          alt="Github stats"
+          class="profile_stats" /> `;
+      } else if (type === "Organization") {
+        console.log("Organization Github profile");
+      }
     })
     .catch((error) => {
       console.log("Error:", error);
     });
 }
+
+// Send a message from the popup to the content script and receive a response
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.sendMessage(
+    tabs[0].id,
+    { query: "get-user" },
+    function (response) {
+      const user = response?.user;
+      console.log("User :", user);
+
+      if (user) {
+        displayUserStats(user);
+      }
+    }
+  );
+});
